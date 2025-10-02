a7187b684ab1833c5507f21449909fac0b53ee6e
pathma2005 <pathmasri1372005@gmail.com>
2025-10-02 19:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
- (function () {
-            'use strict'
+<script>
+  // Bootstrap validation script
+  (function () {
+    'use strict';
+    const form = document.getElementById('internship-form');
 
-            // Get all forms we want to apply custom Bootstrap validation styles to
-            var forms = document.querySelectorAll('.needs-validation')
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
 
-            // Loop over them and prevent submission
-            Array.prototype.slice.call(forms)
-                .forEach(function (form) {
-                    form.addEventListener('submit', function (event) {
-                        if (!form.checkValidity()) {
-                            event.preventDefault()
-                            event.stopPropagation()
-                        }
-                        form.classList.add('was-validated')
-                    }, false)
-                })
-        })()
+      form.classList.add('was-validated');
+    });
+  })();
+</script>
